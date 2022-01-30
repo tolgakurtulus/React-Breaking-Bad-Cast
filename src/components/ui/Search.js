91b01c4ad10767ee0c8433363a9bec7e64b97088
tolga.kurtulus <tolga.kurtulus@odeontechnology.com>
2022-01-30 18:11:06 +0300

    first commit

--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,27 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Search = ({ getQuery }) => {
-  const [text, setText] = useState('')
+  const [text, setText] = useState("");
 
   const onChange = (q) => {
-    setText(q)
-    getQuery(q)
-  }
+    setText(q);
+    getQuery(q);
+  };
 
   return (
-    <section className='search'>
+    <section className="search">
       <form>
         <input
-          type='text'
-          className='form-control'
-          placeholder='Search characters'
+          type="text"
+          className="form-control"
+          placeholder="Search characters"
           value={text}
           onChange={(e) => onChange(e.target.value)}
           autoFocus
         />
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
