e4840c346ae2fd6cb736e14ec642a23afa28a4ce
tolga.kurtulus <tolga.kurtulus@odeontechnology.com>
2022-01-31 09:10:19 +0300

    Updates

---
+++ precache-manifest.8e36c4865d5500c605d15dba063c014d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cbb5800e6ebe57f91398390173268af",
+    "url": "/React-Breaking-Bad-Cast/index.html"
+  },
+  {
+    "revision": "d39c9c2aeb025b687f95",
+    "url": "/React-Breaking-Bad-Cast/static/css/main.45219df1.chunk.css"
+  },
+  {
+    "revision": "0095b81c1e979fa9d7e7",
+    "url": "/React-Breaking-Bad-Cast/static/js/2.4c6339a9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Breaking-Bad-Cast/static/js/2.4c6339a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d39c9c2aeb025b687f95",
+    "url": "/React-Breaking-Bad-Cast/static/js/main.26ebb280.chunk.js"
+  },
+  {
+    "revision": "d87f69d3f397b15f9034",
+    "url": "/React-Breaking-Bad-Cast/static/js/runtime-main.07a0b735.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/React-Breaking-Bad-Cast/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/React-Breaking-Bad-Cast/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/React-Breaking-Bad-Cast/static/media/spinner.235b9b3e.gif"
+  }
+]);
